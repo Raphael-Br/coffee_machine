1604af455b82b1dc76cafcc94dfadbf47a192fd3
Raphael Br <raphi.brunschweiler@gmail.com>
2021-03-17 10:19:05 +0100

    finished

--- Problems/Piggy bank/main.py
+++ Problems/Piggy bank/main.py
@@ -4,13 +4,6 @@ class PiggyBank:
         self.cents = cents
 
     def add_money(self, deposit_dollars, deposit_cents):
-        new_balance = self.cents + deposit_cents + self.dollars * 100 + deposit_dollars * 100
+        new_balance = self.cents + deposit_cents + (self.dollars + deposit_dollars) * 100
         self.dollars = new_balance // 100
         self.cents = new_balance % 100
-
-
-# myObj = PiggyBank(1, 1)
-# print(myObj.dollars, myObj.cents)
-#
-# myObj.add_money(500, 500)
-# print(myObj.dollars, myObj.cents)
